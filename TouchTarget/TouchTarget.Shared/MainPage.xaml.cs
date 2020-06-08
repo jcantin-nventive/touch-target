e2559ac63354b49613aff05d6cae2f94079d7b1d
Julie Cantin <julie.cantin@nventive.com>
2020-06-08 09:45:09 -0400

    Add hyperlink button

--- TouchTarget/TouchTarget.Shared/MainPage.xaml.cs
+++ TouchTarget/TouchTarget.Shared/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -13,18 +14,19 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace TouchTarget
 {
-	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
-	/// </summary>
 	public sealed partial class MainPage : Page
 	{
 		public MainPage()
 		{
 			this.InitializeComponent();
+			btnHyperlink.Click += BtnClicked;
+		}
+
+		private void BtnClicked(object sender, RoutedEventArgs args)
+		{
+			Debugger.Break();
 		}
 	}
 }
